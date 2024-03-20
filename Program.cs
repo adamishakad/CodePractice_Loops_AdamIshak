256231c161c80fca78c8e4630a25f7d58a1bf4ce
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-03-19 17:30:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+namespace CodePractice_Loops_AdamIshak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingVariable = 20;
+            Random random = new Random();
+            int loopCount = 1;
+            while (startingVariable > 0 && startingVariable < 50)
+            {
+                int randomNumber = random.Next(0, 10);
+
+            }
+            if (startingVariable  % 2 == 0) ;
+            {
+                startingVariable -= 5;
+            }
+            
+            //else 
+            {
+                startingVariable += 2;
+                Console.WriteLine($"Loop:{loopCount}, RandomNumber:");
+            }
+
+            
+                
+        }
+    }
+}
